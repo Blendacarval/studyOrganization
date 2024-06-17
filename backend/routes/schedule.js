0b43aee3c9ac242deb22f31c00a7f960c0c1d6b7
profleolima <eletivaete@gmail.com>
2024-06-17 14:14:27 -0300

    1.0
    troca do código base

--- backend/routes/schedule.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createSchedule, getSchedules } = require('../controllers/scheduleController');
-const { authenticate } = require('../middleware/auth');
-const router = express.Router();
-
-router.post('/', authenticate, createSchedule);
-router.get('/', authenticate, getSchedules);
-
-module.exports = router;
